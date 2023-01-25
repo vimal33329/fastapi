ad1bf6ac16f1ad1ac11683b9094bb50f0983235d
Venkatesh S <75470740+venkateshs865@users.noreply.github.com>
2023-01-25 16:42:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from accountsummary.API import account
 
 app = FastAPI()
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(account.router)
-
